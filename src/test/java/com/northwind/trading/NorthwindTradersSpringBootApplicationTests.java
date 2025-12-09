7323ae764df0b3745b5f024304fe2e2119ef025e
Owner <fevzifd2023@gmail.com>
2025-12-09 11:57:41 -0500

    initial commit

---
+++ src/test/java/com/northwind/trading/NorthwindTradersSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northwind.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindTradersSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
