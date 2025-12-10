783709058e51b2e0bfd3b58d35f301f7b5672bca
Owner <fevzifd2023@gmail.com>
2025-12-10 11:16:15 -0500

    Run program and check beans

--- src/main/java/com/northwind/trading/NorthwindTradersSpringBootApplication.java
+++ src/main/java/com/northwind/trading/NorthwindTradersSpringBootApplication.java
@@ -2,12 +2,26 @@ package com.northwind.trading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class NorthwindTradersSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
+		ApplicationContext context= SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
+
+
+        String[] beanNames = context.getBeanDefinitionNames();
+
+        System.out.println("\nMy Application Beans:");
+        for (String beanName : beanNames) {
+            Object bean = context.getBean(beanName);
+            String packageName = bean.getClass().getPackageName();
+
+            if (packageName.startsWith("com.northwind.trading")) {
+                System.out.println(beanName + " -> " + bean.getClass().getSimpleName());
+            }
+        }
 	}
 
 }
